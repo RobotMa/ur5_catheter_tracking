e49c45310dd47efa629468f9af72b398b07d2b95
Qianli Ma <mqianli1@jhu.edu>
2014-09-15 00:59:35 -0400

    cpp change commited

--- me530646_lab1/src/lab1.cpp
+++ me530646_lab1/src/lab1.cpp
@@ -8,39 +8,62 @@
  * Returns the 3x3 rotation matrix that represents a rotation about 
  * the x axis (roll) of phi radians.
  */
-Eigen::Matrix3f rollr(double phi){
-	//TODO
-}
+ Eigen::Matrix3f rollr(double phi){
+
+ 	Eigen::Matrix3f R;
+ 	R << 1,        0,         0,
+ 	     0, cos(phi), -sin(phi),
+ 	     0, sin(phi),  cos(phi);
+ 	return R;
+ }
 
 /**
  * Returns the 3x3 rotation matrix that represents a rotation about 
  * the y axis (pitch) of theta radians.
  */
-Eigen::Matrix3f pitchr(double theta){
-	//TODO		 
-}
+ Eigen::Matrix3f pitchr(double theta){
+
+ 	Eigen::Matrix3f R;
+ 	R << cos(theta),  0,  sin(theta),
+ 	              0,  1,           0,
+ 	    -sin(theta),  0,  cos(theta);
+ 	return R;		 
+ }
 
 /**
  * Returns the 3x3 rotation matrix that represents a rotation about 
  * the z axis (yaw) of psi radians.
  */
-Eigen::Matrix3f yawr(double psi){
-	//TODO
-}
+ Eigen::Matrix3f yawr(double psi){
+
+ 	Eigen::Matrix3f R;
+ 	R << cos(psi),  -sin(psi),  0,
+ 	     sin(psi),   cos(psi),  0,
+ 	            0,          0,  1;
+ 	return R;
+ }
 
 /**
- * Returns the 3x3 rotation matrix that represents successive 
- * roll, pitch, and yaw rotations.
+ * returns the 3x3 rotation matrix that represents successive 
+ * roll, pitch, and yaw rotations. (Should be yaw->pitch->roll)
  */
 Eigen::Matrix3f rpyr(double roll, double pitch, double yaw){
-	//TODO
+	
+	Eigen::Matrix3f R;
+	R = yawr(yaw)*pitchr(pitch)*rollr(roll);
+	return R;
 }
 
 /**
  * Returns the roll, pitch, and yaw from the given 3x3 rotation matrix.
  */
-Eigen::Vector3f rpyrinv(Eigen::Matrix3f r){
-	//TODO
+Eigen::Vector3f rpyrinv(Eigen::Matrix3f R){
+	
+	Eigen::Vector3f v;
+	v(0) = atan2(  R(2,1), R(2,2) );
+	v(1) = atan2( -R(2,0), sqrt( pow(R(2,1),2) + pow(R(2,2),2)) );
+	v(2) = atan2(  R(1,0), R(0,0) );
+	return v;
 }
 
 /**
@@ -48,23 +71,52 @@ Eigen::Vector3f rpyrinv(Eigen::Matrix3f r){
  * translation of (x,y,z) and a rotation of (roll,pitch,yaw).
  */
 Eigen::Matrix4f xf(double x, double y, double z, double roll, double pitch, double yaw){
-	//TODO
+	
+	Eigen::Matrix4f T = Eigen::Matrix4f::Identity(4,4);
+	T.block<3,3>(0,0) = rpyr(roll, pitch, yaw);
+	T.block<3,1>(0,3) << x, y, z;
+	return T; 
 }
 
 /**
  * Returns the matrix inverse to the given homogeneous transformation.
  */
-Eigen::Matrix4f finv(Eigen::Matrix4f f){
-	//TODO
+Eigen::Matrix4f finv(Eigen::Matrix4f T){
+	
+	Eigen::Matrix4f invT = Eigen::Matrix4f::Identity(4,4);
+	Eigen::Matrix3f R = T.block<3,3>(0,0);
+	Eigen::Vector3f t = T.block<3,1>(0,2);
+	Eigen::Matrix3f R_t = R.transpose();
+	invT.block<3,3>(0,0) = R_t;
+	invT.block<3,1>(0,3) = -R_t*t;
+	return invT;
 }
 
 /**
  * Animates a roll-pitch-yaw rotation about the world
  * coordinate frame
  */
-void XYZFixedPlot(double roll, double pitch, double yaw){
-	//TODO
+ void XYZFixedPlot(RvizPlotter& p, double roll, double pitch, double yaw){
 
+ 	const int n = 50;
+ 	Eigen::Matrix4f T_fixed = Eigen::Matrix4f::Identity(4,4);
+ 	for (int i = 0; i < n+1; ++i)
+ 	{
+ 		T_fixed.block<3,3>(0,0) = rollr(roll/n*i);
+ 		p.plotf(T_fixed, "Frame");
+ 	}
+
+ 	for (int j = 0; j < n+1; ++j)
+ 	{
+ 		T_fixed.block<3,3>(0,0) = pitchr(pitch/n*j)*rollr(roll);
+ 		p.plotf(T_fixed, "Frame");
+ 	}
+
+ 	for (int k = 0; k < n+1; ++k)
+ 	{
+ 		T_fixed.block<3,3>(0,0) = yawr(yaw/n*k)*pitchr(pitch)*rollr(roll);
+ 		p.plotf(T_fixed, "Frame");
+ 	}
 	//Hint:First apply the roll rotation in increments,
 	//using plotf to draw the frame in rviz
 }
@@ -73,8 +125,27 @@ void XYZFixedPlot(double roll, double pitch, double yaw){
  * Animates a roll-pitch-yaw rotation about the body-fixed
  * coordinate frame
  */
-void ZYXRelativePlot(double roll, double pitch, double yaw){
-	//TODO
+void ZYXRelativePlot(RvizPlotter& p, double roll, double pitch, double yaw){
+
+	const int n = 50;
+	Eigen::Matrix4f T_fixed = Eigen::Matrix4f::Identity(4,4);
+	for (int i = 0; i < n+1; ++i)
+	{
+		T_fixed.block<3,3>(0,0) = yawr(yaw/n*i);
+		p.plotf(T_fixed, "Frame");
+	}
+
+	for (int j = 0; j < n+1; ++j)
+	{
+		T_fixed.block<3,3>(0,0) = yawr(yaw)*pitchr(pitch/n*j);
+		p.plotf(T_fixed, "Frame");
+	}
+
+	for (int k = 0; k < n+1; ++k)
+	{
+		T_fixed.block<3,3>(0,0) = yawr(yaw)*pitchr(pitch)*rollr(roll/n*k);
+		p.plotf(T_fixed, "Frame");
+	}
 }
 
 
@@ -85,9 +156,9 @@ void ZYXRelativePlot(double roll, double pitch, double yaw){
  */
 Eigen::Matrix4f roll4(double roll)
 {
-	Eigen::Matrix4f r = Eigen::MatrixXf::Identity(4,4);
-	r.block<3,3>(0,0) = rollr(roll);
-	return r;
+	Eigen::Matrix4f T = Eigen::MatrixXf::Identity(4,4);
+	T.block<3,3>(0,0) = rollr(roll);
+	return T;
 }
 
 /**
@@ -96,9 +167,9 @@ Eigen::Matrix4f roll4(double roll)
  */
 Eigen::Matrix4f pitch4(double pitch)
 {
-	Eigen::Matrix4f r = Eigen::MatrixXf::Identity(4,4);
-	r.block<3,3>(0,0) = pitchr(pitch);
-	return r;
+	Eigen::Matrix4f T = Eigen::MatrixXf::Identity(4,4);
+	T.block<3,3>(0,0) = pitchr(pitch);
+	return T;
 }
 
 /**
@@ -107,7 +178,7 @@ Eigen::Matrix4f pitch4(double pitch)
  */
 Eigen::Matrix4f yaw4(double yaw)
 {
-	Eigen::Matrix4f r = Eigen::MatrixXf::Identity(4,4);
-	r.block<3,3>(0,0) = yawr(yaw);
-	return r;
+	Eigen::Matrix4f T = Eigen::MatrixXf::Identity(4,4);
+	T.block<3,3>(0,0) = yawr(yaw);
+	return T;
 }
--- me530646_lab1/src/lab1_main.cpp
+++ me530646_lab1/src/lab1_main.cpp
@@ -16,12 +16,35 @@ int main(int argc, char **argv){
 	
 	/*****Examples of how to use necessary functions*****/
 	//Creates an 4x4 identity matrix
-	Eigen::Matrix4f h = Eigen::MatrixXf::Identity(4,4);
+	Eigen::Matrix4f T0 = Eigen::MatrixXf::Identity(4,4);
 	//Plot the identity matrix in rviz
-	p.plotf(h,"Frame");
+	p.plotf(T0,"Frame");
 	//Print out the matrix
-	printEigen(h);
+	printEigen(T0);
 	//Plot a vector
 	p.plotv("map", Eigen::Vector3f(0,0,0), Eigen::Vector3f(2,4,1));
 	/****************************************************/
+
+	Eigen::Matrix4f T10 = xf(1, 1, 0, 0, 0, PI/2);
+	printEigen(T10);
+	p.plotf(T10,"Frame","Frame1");
+
+	Eigen::Matrix4f T21 = xf(-1, 1, 0, PI/2, PI/2, PI/2);
+	printEigen(T21);
+	p.plotf(T21,"Frame","Frame2");
+
+	Eigen::Matrix3f rpyR = T21.block<3,3>(0,0);
+	Eigen::Vector3f rpy = rpyrinv(rpyR);
+	printEigen(rpy);
+
+	Eigen::Matrix3f R = rpyr(rpy(0), rpy(1), rpy(2));
+	printEigen(R);
+	Eigen::Matrix4f T = Eigen::Matrix4f::Identity(4,4);
+	T.block<3,3>(0,0) = R;
+	p.plotf(T,"Frame","Test");
+
+	XYZFixedPlot(p, PI/3, PI/4, PI/3);
+	sleep(2);
+	ZYXRelativePlot(p, PI/3, PI/4, PI/3);
+
 }
