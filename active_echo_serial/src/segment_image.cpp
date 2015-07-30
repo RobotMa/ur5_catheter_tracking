d2320c5956add75ece66d2a4faaa8dadd1108efa
Qianli Ma <mqianli1@jhu.edu>
2015-07-30 17:56:07 -0400

    Create a separate branch for active_echo debugging

--- active_echo_serial/src/segment_image.cpp
+++ active_echo_serial/src/segment_image.cpp
@@ -15,46 +15,46 @@ void segmentCallback(const active_echo_serial::Num::ConstPtr& msg)
 {
 	static tf::TransformBroadcaster br;
 	tf::Transform transform;
-	bool mid_plane = true;
+	bool mid_plane = false;
 
-    double element_w = 0.45; // mm
-    double AE_SRate = 80*pow(10, 6); // hz
-    double SOS = 1480; // m/s
+	double element_w = 0.45; // mm
+	double AE_SRate = 80*pow(10, 6); // hz
+	double SOS = 1480; // m/s
 
-    // Linear ultrasound probe
-    // Note: x and y are flipped so that the reference frame of the probe
-    // and the robot based will be parallel to each while at working status
-    double x = 0.0;
-    if (mid_plane == true)
-    {
-    	x = 0.0; // Assume that the segmented point falls within the mid-plane
-    }
-    else
-    {
-	// Gaussian fitting paramters a, b and c; Currently calculated from the gaussianFitting.m file 
-    	double a = 39.6629;
-    	double b = -0.0277;
-    	double c = 3.1947; 
-    	x = sqrt(-pow(c,2)*log(msg->tc/a)) + b;
-    }
-    double y = ( msg->l_ta - 64.5)*element_w/1000; // Unit:m
-    double z = -(msg->dly)*(1/AE_SRate)*SOS; // Unit:m
+	// Linear ultrasound probe
+	// Note: x and y are flipped so that the reference frame of the probe
+	// and the robot based will be parallel to each while at working status
+	double x = 0.0;
+	if (mid_plane == true)
+	{
+		x = 0.0; // Assume that the segmented point falls within the mid-plane
+	}
+	else
+	{
+		// Gaussian fitting paramters a, b and c; Currently calculated from the gaussianFitting.m file 
+		double a = 39.6629;
+		double b = -0.0277;
+		double c = 3.1947; 
+		x = sqrt(-pow(c,2)*log(msg->tc/a)) + b;
+	}
+	double y = ( msg->l_ta - 64.5)*element_w/1000; // Unit:m
+	double z = -(msg->dly)*(1/AE_SRate)*SOS; // Unit:m
 
-//    geometry_msgs::Point segment_point;
-//    segment_point.x = x;
-//    segment_point.y = y;
-//    segment_point.z = z;
-    transform.setOrigin( tf::Vector3(x, y, z));
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"ultrasound_sensor", "segment_point"));
+	//    geometry_msgs::Point segment_point;
+	//    segment_point.x = x;
+	//    segment_point.y = y;
+	//    segment_point.z = z;
+	transform.setOrigin( tf::Vector3(x, y, z));
+	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"ultrasound_sensor", "segment_point"));
 
 
-    // Spherical ultrasound probe
-//    int offset = -290;
+	// Spherical ultrasound probe
+	//    int offset = -290;
 
-//    double angle = ( (msg->l_ta - 129/2)/129)*80;
-//    double radius = 1000*(msg->dly + offset)*(1/(AE_SRate))*SOS;
-//    x = radius*sind(angle); // sind is a triangular function
-//    z = radius*cosd(angle); // cosd is a triangular function
+	//    double angle = ( (msg->l_ta - 129/2)/129)*80;
+	//    double radius = 1000*(msg->dly + offset)*(1/(AE_SRate))*SOS;
+	//    x = radius*sind(angle); // sind is a triangular function
+	//    z = radius*cosd(angle); // cosd is a triangular function
 
 }
 
--- active_echo_serial/src/segment_pose_publisher.cpp
+++ active_echo_serial/src/segment_pose_publisher.cpp
@@ -15,9 +15,12 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "segment_pose_publisher");
 	ros::NodeHandle n;
 
-	ros::Publisher pub_pose = n.advertise<geometry_msgs::Pose>( "set_pose", 1);
+	ros::Publisher pub_pose = n.advertise<geometry_msgs::Pose>( "setpose", 1);
 
-	tf::TransformListener listener;
+	tf::TransformListener listener_1;
+	tf::TransformListener listener_2; // Add a 2nd transform listener trying to fix messed up tf
+
+	ros::Rate r(10); // 10 Hz 
 
 	while (n.ok()){
 
@@ -25,8 +28,8 @@ int main(int argc, char **argv)
 		tf::Transform transform_be;
 
 		try{
-        // Calculate the new pose of /ee_link based on the pose /segment_point
-        // Look up the transformation between /ee_link and /base_link
+        	// Calculate the new pose of /ee_link based on /segment_point
+        	// Look up the transformation between /ee_link and /base_link
 			std::string ref_frame( "base_link" );
 			std::string tge_frame( "ee_link" );
 			std::string tgs_frame( "segment_point" );
@@ -35,18 +38,38 @@ int main(int argc, char **argv)
 
 			tf::StampedTransform transform_ue;
 			tf::StampedTransform transform_bs;
-			listener.waitForTransform( tge_frame, tgu_frame, ros::Time(0), ros::Duration(0.8) );
-			listener.lookupTransform( tge_frame, tgu_frame, ros::Time(0), transform_ue );
-			listener.waitForTransform( ref_frame, tgs_frame, ros::Time(0), ros::Duration(0.8)  );
-			listener.lookupTransform( ref_frame, tgs_frame, ros::Time(0), transform_bs );
+			listener_1.waitForTransform( tge_frame, tgu_frame, ros::Time(0), ros::Duration(0.8) );
+			listener_1.lookupTransform( tgu_frame, tge_frame, ros::Time(0), transform_ue );
+			listener_2.waitForTransform( ref_frame, tgs_frame, ros::Time(0), ros::Duration(0.8)  );
+			listener_2.lookupTransform( ref_frame, tgs_frame, ros::Time(0), transform_bs );
+
+
+			// debug nan x, y, z of des_pose
+			std::cout << transform_bs.getOrigin().x() << std::endl;
+			std::cout << transform_bs.getOrigin().y() << std::endl;	
 
 			transform_be.mult(transform_bs, transform_ue);
 
+			geometry_msgs::Pose des_pose;
+
+			des_pose.position.x =  transform_be.getOrigin().x();
+			des_pose.position.y =  transform_be.getOrigin().y();
+			des_pose.position.z =  transform_be.getOrigin().z();
+
+			des_pose.orientation.x = transform_be.getRotation().x();
+			des_pose.orientation.y = transform_be.getRotation().y();
+			des_pose.orientation.z = transform_be.getRotation().z();
+			des_pose.orientation.w = transform_be.getRotation().w();
+
+			pub_pose.publish( des_pose );
 
 		}
 		catch(tf::TransformException ex)
 		{ std::cout << ex.what() << std::endl; }
 
+		// tf is messed up 	
+
+		/*
 		geometry_msgs::Pose des_pose;
 
 		des_pose.position.x =  transform_be.getOrigin().x();
@@ -57,10 +80,13 @@ int main(int argc, char **argv)
 		des_pose.orientation.y = transform_be.getRotation().y();
 		des_pose.orientation.z = transform_be.getRotation().z();
 		des_pose.orientation.w = transform_be.getRotation().w();
-
+		
 
 		pub_pose.publish( des_pose );	
+		*/
 		ros::spin();
+
+		r.sleep();
 
 	}
 
